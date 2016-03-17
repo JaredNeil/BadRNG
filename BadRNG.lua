5b7f584ab62f4e7be6fc7442cc48b01af1f12023
Jared Neil <jaredaneil@gmail.com>
2016-03-17 12:27:38 -0600

    Initial Commit

---
+++ BadRNG.lua
@@ -0,0 +1,55 @@
+local parser = ParserLib:GetInstance("1.1")
+
+local skills = {
+  "Heroic Strike",
+  "Shield Slam",
+  "Sunder Armor",
+  "Revenge"
+}
+
+local tanks = {
+  "Ahtia",
+  "Sulg",
+  "Henkebenke",
+  "Nyo",
+  "Percaso"
+}
+
+local bosses = {
+  "Vaelastrasz",
+  "Broodlord Lashlayer",
+  "Chromagus"
+}
+
+function contains(list, item)
+  for _, i in ipairs(list) do
+    if item == i then
+      return true
+    end
+  end
+  return false
+end
+
+
+function BadRNG_OnLoad()
+  parser:RegisterEvent("BadRNG", "CHAT_MSG_SPELL_PARTY_DAMAGE", ParserLib_OnEvent)
+  -- parser:RegisterEvent("BadRNG", "CHAT_MSG_SPELL_SELF_DAMAGE", ParserLib_OnEvent)
+end
+
+function ParserLib_OnEvent(event, info)
+  if info.type == "miss" then
+    if contains(tanks, info.source) then
+      if contains(skills, info.skill) then
+        local boss_match = false
+        for _, boss in ipairs(bosses) do
+          if string.find(info.victim, boss) then
+            boss_match = true
+          end
+        end
+        if boss_match then
+          -- Everything matches. Do what you want here.
+        end
+      end
+    end
+  end
+end
